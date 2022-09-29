f6d34bebd47f3330b165b7089a89fc28d4fe3436
soygagi <101847092+soygagi@users.noreply.github.com>
2022-09-29 17:45:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+    
+}
+function setup(){
+    Canvas = createCanvas(300,300);
+   canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('SPOOKIES.png');
+}
